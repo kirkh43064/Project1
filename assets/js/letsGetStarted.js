0874e88e929a9e1e22373130ccf5314bd8d7f781
mollenro <mollenro@gmail.com>
2021-09-09 18:59:24 -0400

    storing updates

--- assets/js/letsGetStarted.js
+++ assets/js/letsGetStarted.js
@@ -1,6 +1,6 @@
-let submitBtn = document.getElementById("submit");
 let requestUrl = "https://wger.de/api/v2/muscle/?format=json";
-let selectMenu = document.getElementById('dropdown')
+let submitBtn = document.getElementById('submit');
+let selectMenu = document.getElementById('dropdown');
 let userChoice;
 
 fetch(requestUrl)
@@ -10,7 +10,7 @@ fetch(requestUrl)
   .then(function (data) {
     //Loops through the data creating options in the drop-down menu for each muscle group found in the API
     for (let i = 0; i < data.results.length; i++) {
-      let optionEl = document.createElement("option");
+      let optionEl = document.createElement('option');
       optionEl.value = data.results[i].name.toLowerCase().replace(/ /g, '');
       optionEl.innerHTML = data.results[i].name;
       selectMenu.appendChild(optionEl);
@@ -20,5 +20,5 @@ fetch(requestUrl)
 submitBtn.addEventListener('click', function(){
     //Logs the user's choice in the local storage for future reference
     userChoice = selectMenu.options[selectMenu.selectedIndex].text;
-    localStorage.setItem("Muscle Group", userChoice);
+    localStorage.setItem('Muscle Group', userChoice);
 })
